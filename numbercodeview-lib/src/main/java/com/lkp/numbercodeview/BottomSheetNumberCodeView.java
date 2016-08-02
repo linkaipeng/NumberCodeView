ba57d51d5d81d0e0c07331e65040048987bd0ba4
linkaipeng <214716003@qq.com>
2016-08-02 16:24:25 +0800

    [modify] Perfected the bottom sheet style number code view. Add number code view show and hide animation. Make the logic of view more complete.

--- numbercodeview-lib/src/main/java/com/lkp/numbercodeview/BottomSheetNumberCodeView.java
+++ numbercodeview-lib/src/main/java/com/lkp/numbercodeview/BottomSheetNumberCodeView.java
@@ -1,14 +1,23 @@
 package com.lkp.numbercodeview;
 
+import android.animation.Animator;
+import android.animation.ObjectAnimator;
 import android.content.Context;
 import android.util.AttributeSet;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 
 /**
  * Created by linkaipeng on 16/8/2.
  */
-public class BottomSheetNumberCodeView extends BaseNumberCodeView {
+public class BottomSheetNumberCodeView extends BaseNumberCodeView implements View.OnClickListener {
+
+    private LinearLayout mBottomNumberCodeLayout;
+    private ImageView mCloseImageView;
+    private OnHideBottomLayoutListener mOnHideBottomLayoutListener;
+    private float mDisplayHeight;
 
     public BottomSheetNumberCodeView(Context context) {
         super(context, null);
@@ -16,15 +25,92 @@ public class BottomSheetNumberCodeView extends BaseNumberCodeView {
 
     public BottomSheetNumberCodeView(Context context, AttributeSet attrs) {
         super(context, attrs);
+        mDisplayHeight = context.getResources().getDisplayMetrics().heightPixels;
     }
 
     @Override
     protected View createView() {
-        return LayoutInflater.from(mContext).inflate(R.layout.view_bottom_sheet_input_code, null);
+        View view = LayoutInflater.from(mContext).inflate(R.layout.view_bottom_sheet_input_code, null);
+        mBottomNumberCodeLayout = (LinearLayout) view.findViewById(R.id.bottom_number_code_layout);
+        mCloseImageView = (ImageView) view.findViewById(R.id.close_bottom_number_code_view);
+        mCloseImageView.setOnClickListener(this);
+        return view;
     }
 
     @Override
     protected void onResult(String code) {
+        if (mCallback != null) {
+            mCallback.onResult(code);
+        }
+        hideNumberCodeLayout();
+    }
 
+    public boolean isNumberCodeLayoutShowing(){
+        if (mBottomNumberCodeLayout != null) {
+            return mBottomNumberCodeLayout.getVisibility() == View.VISIBLE;
+        } else {
+            return false;
+        }
+    }
+
+    public void showNumberCodeLayout(){
+        if (mBottomNumberCodeLayout == null) {
+            return;
+        }
+        mBottomNumberCodeLayout.setVisibility(VISIBLE);
+        float yTranslation = mBottomNumberCodeLayout.getTranslationY();
+        ObjectAnimator yTranslationAnimator = ObjectAnimator.ofFloat(mBottomNumberCodeLayout, "translationY", mDisplayHeight, yTranslation);
+        yTranslationAnimator.setDuration(500);
+        yTranslationAnimator.start();
+    }
+
+    public void hideNumberCodeLayout(){
+        if (mBottomNumberCodeLayout == null) {
+            return;
+        }
+        float yTranslation = mBottomNumberCodeLayout.getTranslationY();
+        ObjectAnimator yTranslationAnimator = ObjectAnimator.ofFloat(mBottomNumberCodeLayout, "translationY", yTranslation, mDisplayHeight);
+        yTranslationAnimator.setDuration(400);
+        yTranslationAnimator.addListener(new Animator.AnimatorListener() {
+            @Override
+            public void onAnimationStart(Animator animation) {
+
+            }
+
+            @Override
+            public void onAnimationEnd(Animator animation) {
+                mBottomNumberCodeLayout.setVisibility(GONE);
+                if (mOnHideBottomLayoutListener != null) {
+                    mOnHideBottomLayoutListener.onHide();
+                }
+            }
+
+            @Override
+            public void onAnimationCancel(Animator animation) {
+
+            }
+
+            @Override
+            public void onAnimationRepeat(Animator animation) {
+
+            }
+        });
+        yTranslationAnimator.start();
+    }
+
+    public void setOnHideBottomLayoutListener(OnHideBottomLayoutListener onHideLayoutListener){
+        mOnHideBottomLayoutListener = onHideLayoutListener;
+    }
+
+    @Override
+    public void onClick(View v) {
+        int viewId = v.getId();
+        if (viewId == R.id.close_bottom_number_code_view) {
+            hideNumberCodeLayout();
+        }
+    }
+
+    public interface OnHideBottomLayoutListener {
+        void onHide();
     }
 }
---
+++ numbercodeview-lib/src/main/java/com/lkp/numbercodeview/bottomsheet/BottomSheetNumberCodeViewActivity.java
@@ -0,0 +1,65 @@
+package com.lkp.numbercodeview.bottomsheet;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+import com.lkp.numbercodeview.BaseNumberCodeView;
+import com.lkp.numbercodeview.BottomSheetNumberCodeView;
+import com.lkp.numbercodeview.R;
+
+
+/**
+ * Created by linkaipeng on 16/8/2.
+ */
+public class BottomSheetNumberCodeViewActivity extends AppCompatActivity
+    implements BaseNumberCodeView.OnInputNumberCodeCallback,
+        BottomSheetNumberCodeView.OnHideBottomLayoutListener {
+
+    public static final int REQUEST_CODE_SHOW_BOTTOM_NUMBER_VIEW = 1001;
+    public static final String KEY_DATA_NUMBER = "KeyDataNumber";
+
+    public static void show(Activity activity){
+        Intent intent = new Intent(activity, BottomSheetNumberCodeViewActivity.class);
+        activity.startActivityForResult(intent, REQUEST_CODE_SHOW_BOTTOM_NUMBER_VIEW);
+    }
+
+    private BottomSheetNumberCodeView mNumberCodeView;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bottom_sheet_number_code_view);
+        initView();
+    }
+
+    private void initView(){
+        mNumberCodeView = (BottomSheetNumberCodeView) findViewById(R.id.bottom_sheet_number_code_view);
+        mNumberCodeView.setNumberCodeCallback(this);
+        mNumberCodeView.setOnHideBottomLayoutListener(this);
+        mNumberCodeView.showNumberCodeLayout();
+    }
+
+    @Override
+    public void onResult(String code) {
+        Intent resultIntent = new Intent();
+        resultIntent.putExtra(KEY_DATA_NUMBER, code);
+        setResult(RESULT_OK, resultIntent);
+    }
+
+    @Override
+    public void onHide() {
+        finish();
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (mNumberCodeView.isNumberCodeLayoutShowing()) {
+            mNumberCodeView.hideNumberCodeLayout();
+        } else {
+            super.onBackPressed();
+        }
+    }
+}
